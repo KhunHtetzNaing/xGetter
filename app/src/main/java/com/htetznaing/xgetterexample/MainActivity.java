7615e5b62d14ab4d72c8657f0cab72a4d4f2c42c
Khun Htetz Naing <khunhtetznaing@gmail.com>
2019-02-18 19:21:26 +0630

    1.2

--- app/src/main/java/com/htetznaing/xgetterexample/MainActivity.java
+++ app/src/main/java/com/htetznaing/xgetterexample/MainActivity.java
@@ -1,21 +1,31 @@
 package com.htetznaing.xgetterexample;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.htetznaing.xgetter.XGetter;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
     XGetter xGetter;
     ProgressDialog progressDialog;
     String org;
+    EditText edit_query;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,28 +33,34 @@ public class MainActivity extends AppCompatActivity {
 
         progressDialog = new ProgressDialog(this);
         progressDialog.setCancelable(false);
-        textView = findViewById(R.id.test);
         xGetter = new XGetter(this);
         xGetter.onFinish(new XGetter.OnTaskCompleted() {
             @Override
             public void onTaskCompleted(final String vidURL) {
                 progressDialog.dismiss();
-                textView.setText("Original: "+org+"\n\nResult: "+vidURL);
-                System.out.println("Original: "+org+"\n\nResult: "+vidURL);
+                done("Input\n"+org+"\n\nResult\n"+vidURL);
+            }
+
+            @Override
+            public void onFbTaskCompleted(String sd, String hd) {
+                progressDialog.dismiss();
+                done("Input\n"+org+"\n\nHD\n"+hd+"\n\nSD\n"+sd);
             }
 
             @Override
             public void onError() {
                 progressDialog.dismiss();
-                textView.setText("ERROR");
+                done("ERROR");
             }
         });
+
+        edit_query = findViewById(R.id.edit_query);
     }
 
     private void letGo(String url){
-        progressDialog.show();
         org = url;
         if (checkInternet()) {
+            progressDialog.show();
             xGetter.find(url);
         }
     }
@@ -77,6 +93,18 @@ public class MainActivity extends AppCompatActivity {
         letGo("https://www.rapidvideo.com/v/FY1NF585GW");
     }
 
+    public void gdrive(View view) {
+        letGo("https://drive.google.com/open?id=1BsXhA3Sw64d2S2ZeGr5qDcPCxMRvK5Ch");
+    }
+
+    public void gphotos(View view) {
+        letGo("https://photos.google.com/share/AF1QipPsU5wF954O4FwAaLp1YiTmCBv1c2cHg2dpu53fpVFggA_Ba8N9V0yH2mo7mcEHEQ/photo/AF1QipN2p_PrQiRRKgeg5flVmTaS-NSVbhTsfp1b15cf?key=NU81ZkxmY09NMWp0M01QNlY3Mng5OVA5VXNzUjJn");
+    }
+
+    public void fb(View view) {
+        letGo("https://www.facebook.com/KHtetzNaing/videos/925916414463007/");
+    }
+
     public boolean checkInternet(){
         boolean what = false;
         CheckInternet checkNet = new CheckInternet(this);
@@ -87,5 +115,79 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "No internet connection!", Toast.LENGTH_SHORT).show();
         }
         return what;
+    }
+
+
+    public void done(String message){
+        System.out.printf(message);
+        View view = getLayoutInflater().inflate(R.layout.done,null);
+        TextView textView = view.findViewById(R.id.message);
+        textView.setText(message);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("Done")
+                .setView(view)
+                .setPositiveButton("OK",null);
+        builder.show();
+    }
+
+    public void dev(View view) {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("fb://profile/100030031876000"));
+            startActivity(intent);
+        }catch (Exception e){
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://m.facebook.com/100030031876000"));
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.about){
+            showAbout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void fetch(View view) {
+        String url = edit_query.getText().toString();
+        if (URLUtil.isValidUrl(url)){
+            letGo(url);
+        }else Toast.makeText(this, "Input valid url :)", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showAbout(){
+        String message = "Extract stream/download url!\n" +
+                "\n" +
+                "#Supported Sites\n" +
+                "\n" +
+                "Google Drive\n" +
+                "Google Photos\n" +
+                "Facebook\n" +
+                "Openload\n" +
+                "Streamango\n" +
+                "StreamCherry\n" +
+                "Mp4Upload\n" +
+                "RapidVideo\n" +
+                "SendVid\n" +
+                "VidCloud\n" +
+                "MegaUp\n" +
+                "\n" +
+                "Github Repo => https://github.com/KhunHtetzNaing/xGetter";
+        View view = getLayoutInflater().inflate(R.layout.done,null);
+        TextView textView = view.findViewById(R.id.message);
+        textView.setText(message);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("About")
+                .setView(view)
+                .setPositiveButton("OK",null);
+        builder.show();
     }
 }
--- xgetter/src/main/java/com/htetznaing/xgetter/XGetter.java
+++ xgetter/src/main/java/com/htetznaing/xgetter/XGetter.java
@@ -7,6 +7,18 @@ import android.webkit.DownloadListener;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,7 +32,8 @@ import java.util.regex.Pattern;
  */
 
 public class XGetter {
-    WebView webView;
+    private WebView webView;
+    private Context context;
     private OnTaskCompleted onComplete;
     private final String openload = "https?:\\/\\/(www\\.)?(openload|oload)\\.[^\\/,^\\.]{2,}\\/(embed|f)\\/.+";
     private final String fruits = "https?:\\/\\/(www\\.)?(streamango|fruitstreams|streamcherry|fruitadblock|fruithosts)\\.[^\\/,^\\.]{2,}\\/(f|embed)\\/.+";
@@ -29,9 +42,16 @@ public class XGetter {
     private final String sendvid = "https?:\\/\\/(www\\.)?(sendvid)\\.[^\\/,^\\.]{2,}\\/.+";
     private final String vidcloud = "https?:\\/\\/(www\\.)?(vidcloud|vcstream|loadvid)\\.[^\\/,^\\.]{2,}\\/(v|embed)\\/.+";
     private final String rapidvideo = "https?:\\/\\/(www\\.)?rapidvideo\\.[^\\/,^\\.]{2,}\\/(\\?v=[^&\\?]*|e\\/.+|v\\/.+)";
+    private final String gphoto = "https?:\\/\\/(photos.google.com)\\/(u)?\\/?(\\d)?\\/?(share)\\/.+(key=).+";
+    private final String fb = "(?:https?://)?(?:www.|web.|m.)?facebook.com/(?:video.php\\?v=\\d+|photo.php\\?v=\\d+|\\?v=\\d+)|\\S+/videos/((\\S+)/(\\d+)|(\\d+))/?";
 
     public XGetter(Context view) {
-        webView = new WebView(view);
+        this.context=view;
+        init();
+    }
+
+    private void init(){
+        webView = new WebView(context);
         webView.setWillNotDraw(true);
         webView.addJavascriptInterface(new xJavascriptInterface(),"xGetter");
         webView.getSettings().setJavaScriptEnabled(true);
@@ -106,10 +126,12 @@ public class XGetter {
                     "OwogICAgICAgICAgICB2YXIgZGF0YSA9IHJlcy5odG1sOwogICAgICAgICAgICB2YXIgcmVnZXgg\n" +
                     "PSAvaHJlZj0iKC4qPykiLzsKICAgICAgICAgICAgdmFyIG07CiAgICAgICAgICAgIGlmICgobSA9\n" +
                     "IHJlZ2V4LmV4ZWMoZGF0YSkpICE9PSBudWxsKSB7CiAgICAgICAgICAgICAgICB4R2V0dGVyLmZ1\n" +
-                    "Y2sobVsxXSk7CiAgICAgICAgICAgIH0KICAgICAgICB9CiAgICB9KTsKfQoKLyoKU3VwcG9ydGVk\n" +
-                    "IFNpdGVzCj0+IE9wZW5sb2FkIChBbGwgZG9tYWlucykKPT4gRnJ1aXRTdHJlYW1zIChTdHJlYW1j\n" +
-                    "aGVycnksU3RyZWFtYW5nbyBhbmQgZXRjLi4pCj0+IE1wNFVwbG9hZAo9PiBSYXBpZFZpZGVvCj0+\n" +
-                    "IFNlbmRWaWQKPT4gTWVnYVVwCj0+IFZpZENsb3VkIChBbGwgZG9tYWlucykKKi8=";
+                    "Y2sobVsxXSk7CiAgICAgICAgICAgIH0KICAgICAgICB9CiAgICB9KTsKfSBlbHNlIGlmKHdpbmRv\n" +
+                    "dy5sb2NhdGlvbi5ob3N0ID09ICdkcml2ZS5nb29nbGUuY29tJyl7Cglkb2N1bWVudC5nZXRFbGVt\n" +
+                    "ZW50QnlJZCgndWMtZG93bmxvYWQtbGluaycpLmNsaWNrKCk7Cn0KCi8qClN1cHBvcnRlZCBTaXRl\n" +
+                    "cwo9PiBPcGVubG9hZCAoQWxsIGRvbWFpbnMpCj0+IEZydWl0U3RyZWFtcyAoU3RyZWFtY2hlcnJ5\n" +
+                    "LFN0cmVhbWFuZ28gYW5kIGV0Yy4uKQo9PiBNcDRVcGxvYWQKPT4gUmFwaWRWaWRlbwo9PiBTZW5k\n" +
+                    "VmlkCj0+IE1lZ2FVcAo9PiBWaWRDbG91ZCAoQWxsIGRvbWFpbnMpCiov";
             view.loadUrl("javascript:(function() {" +
                     "var parent = document.getElementsByTagName('head').item(0);" +
                     "var script = document.createElement('script');" +
@@ -121,16 +143,17 @@ public class XGetter {
     }
 
     public void find(String url){
+        boolean fb = false;
         boolean run = false;
         if (check(openload,url)){
-            run = true;
             //Openload
+            run = true;
         }else if (check(fruits,url)){
-            run = true;
             //Fruits
-        }else if (check(megaup,url)){
             run = true;
+        }else if (check(megaup,url)){
             //megaup
+            run = true;
         }else if (check(mp4upload,url)){
             run = true;
             if (!url.contains("embed-")){
@@ -149,22 +172,131 @@ public class XGetter {
             }
 
         }else if (check(sendvid,url)){
-            run = true;
             //sendvid
-        }else if (check(vidcloud,url)){
             run = true;
+        }else if (check(vidcloud,url)){
             //vidcloud
+            run = true;
             if (!url.contains("/embed/") && url.contains("/v/")){
                 url = url.replace("/v/","/embed/");
             }
         }else if (check(rapidvideo,url)){
-            run = true;
             //rapidvideo
+            run = true;
+        }else if (check(gphoto,url)){
+            //gphotos
+            run = true;
+        }else if (url.contains("drive.google.com") && get_drive_id(url)!=null) {
+            //gdrive
+            run = true;
+            url = "https://drive.google.com/uc?id="+get_drive_id(url)+"&export=download";
+        }else if (check_fb_video(url)!=null){
+            //fb
+            run = true;
+            fb = true;
+            url = "https://www.facebook.com/video.php?v="+check_fb_video(url);
         }
 
         if (run) {
-            webView.loadUrl(url);
+            if (check(gphoto,url)){
+                gphotoORfb(url,false);
+            }else if (fb){
+                gphotoORfb(url,true);
+            }else {
+                webView.loadUrl(url);
+            }
         }else onComplete.onError();
+    }
+
+    private String check_fb_video(String url){
+        final Pattern pattern = Pattern.compile(fb);
+        final Matcher matcher = pattern.matcher(url);
+        if (matcher.find()) {
+            String s = matcher.group(1);
+            if (s!=null) {
+                if (s.contains("v=")) {
+                    s = s.substring(s.indexOf("v=") + 2);
+                } else if (s.contains("vb.")) {
+                    for (int i = 1; i <= matcher.groupCount(); i++) {
+                        String temp = matcher.group(i);
+                        if (isNumber(temp)) {
+                            s = temp;
+                        }
+                    }
+                }
+                return s;
+            }
+        }
+        return null;
+    }
+
+    private boolean isNumber(String str){
+        return str.replaceAll("[0-9]","").length() == 0;
+    }
+
+    private String get_drive_id(String string){
+        final String regex = "[-\\w]{25,}";
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(string);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return null;
+    }
+
+    private void gphotoORfb(String url, final boolean fb){
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (fb){
+                    onComplete.onFbTaskCompleted(getFbLink(response,false),getFbLink(response,true));
+                }else {
+                    onComplete.onTaskCompleted(getGPhotoLink(response));
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                onComplete.onError();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("User-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
+                return headers;
+            }
+        };
+
+        Volley.newRequestQueue(context).add(request);
+    }
+
+    private String getGPhotoLink(String string){
+        string = string.replaceAll("%(?![0-9a-fA-F]{2})", "%25");
+        try {
+            string = URLDecoder.decode(string,"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        final String regex = "https:\\/\\/(.*?)=m(22|18|37)";
+        final Pattern pattern = Pattern.compile(regex);
+        final Matcher matcher = pattern.matcher(string);
+        if (matcher.find()){
+            return matcher.group();
+        }
+        return null;
+    }
+
+    private String getFbLink(String source,boolean hdquality){
+        String s=source;
+        String q=(hdquality?"hd_src:\"":"sd_src:\"");
+        int idx=s.indexOf(q);
+        if(idx!=-1){
+            s=s.substring(idx+q.length());
+            return s.substring(0,s.indexOf("\""));
+        }else{
+            return null;
+        }
     }
 
     private boolean check(String regex,String string){
@@ -175,10 +307,16 @@ public class XGetter {
 
     public interface OnTaskCompleted {
         void onTaskCompleted(String vidURL);
+        void onFbTaskCompleted(String sd,String hd);
         void onError();
     }
 
     public void onFinish(OnTaskCompleted onComplete) {
         this.onComplete = onComplete;
+    }
+
+    private void destory(){
+        webView.destroy();
+        webView = null;
     }
 }
